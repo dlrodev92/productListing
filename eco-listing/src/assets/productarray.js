2e0f64c68ff4b1efc5645fa33c8256642428b6e4
Carrie Shen <carrieshen.uk@gmail.com>
2023-08-09 11:27:37 +0100

    setup product array

---
+++ eco-listing/src/assets/productarray.js
@@ -0,0 +1,52 @@
+
+const greenproducts = [
+      {
+        "name": "Organic Bananas",
+        "id": 101,
+        "sustainable": true,
+        "seasonal": false,
+        "local": false,
+        "eco_friendly_green_point": 8,
+        "discount": 0.15,
+        "url": //assets.iceland.co.uk/i/iceland/fyffes_premium_bananas_89322_T1.jpg
+      },
+      {
+        "name": "Grass-fed Beef",
+        "id": 102,
+        "sustainable": false,
+        "seasonal": true,
+        "local": false,
+        "eco_friendly_green_point": 9,
+        "discount": 0.10
+      },
+      {
+        "name": "Quinoa Salad",
+        "id": 103,
+        "sustainable": false,
+        "seasonal": true,
+        "local": true,
+        "eco_friendly_green_point": 7,
+        "discount": 0.20
+      },
+      {
+        "name": "Free-range Eggs",
+        "id": 104,
+        "sustainable": true,
+        "seasonal": false,
+        "local": true,
+        "eco_friendly_green_point": 6,
+        "discount": 0.10
+      },
+      {
+        "name": "Whole Grain Bread",
+        "id": 105,
+        "sustainable": true,
+        "seasonal": true,
+        "local": true,
+        "eco_friendly_green_point": 7,
+        "discount": 0.15
+      }
+    ]
+
+    export default greenproducts;
+  
