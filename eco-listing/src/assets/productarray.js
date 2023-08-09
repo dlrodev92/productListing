d922cbb0b725353225343b4f40ae32bd8f8569b7
Carrie Shen <carrieshen.uk@gmail.com>
2023-08-09 11:45:28 +0100

    greenproductx10 carrie

--- eco-listing/src/assets/productarray.js
+++ eco-listing/src/assets/productarray.js
@@ -1,52 +1,126 @@
+const products = [
+    {
+      "name": "Organic Bananas",
+      "id": 101,
+      "price": 1.00,
+      "green": true,
+      "sustainable": true,
+      "seasonal": false,
+      "local": false,
+      "eco_friendly_green_point": 8,
+      "discount": 0.15,
+      "url": "//assets.iceland.co.uk/i/iceland/fyffes_premium_bananas_89322_T1.jpg"
+    },
+    {
+      "name": "Grass-fed Beef",
+      "id": 102,
+      "price": 5.50,
+      "green": true,
+      "sustainable": false,
+      "seasonal": true,
+      "local": false,
+      "eco_friendly_green_point": 9,
+      "discount": 0.10,
+      "url": "//assets.iceland.co.uk/i/iceland/iceland_big_daddy_sirloin_steak_340g_81297.jpg"
+    },
+    {
+      "name": "Fresh Red and Green Salad",
+      "id": 103,
+      "price": 2.50,
+      "green": true,
+      "sustainable": false,
+      "seasonal": true,
+      "local": true,
+      "eco_friendly_green_point": 7,
+      "discount": 0.20,
+      "url": "//assets.iceland.co.uk/i/iceland/fresh_leaf_co_red_green_mix_70g_94327_T1"
+    },
+    {
+      "name": "Free-range Eggs",
+      "id": 104,
+      "price": 4.25,
+      "green": true,
+      "sustainable": true,
+      "seasonal": false,
+      "local": true,
+      "eco_friendly_green_point": 6,
+      "discount": 0.10,
+      "url": "//assets.iceland.co.uk/i/iceland/iceland_12_large_free_range_eggs_64257.jpg"
+    },
+    {
+      "name": "Whole Grain Bread",
+      "id": 105,
+      "price": 5.75,
+      "green": true,
+      "sustainable": true,
+      "seasonal": true,
+      "local": true,
+      "eco_friendly_green_point": 7,
+      "discount": 0.15,
+      "url": "//assets.iceland.co.uk/i/iceland/iceland_thick_sliced_wholemeal_800g_68906.jpg?"
+    },
+    {
+      "name": "Wild-Caught Salmon",
+      "id": 106,
+      "price": 9.99,
+      "green": true,
+      "sustainable": true,
+      "seasonal": false,
+      "local": false,
+      "eco_friendly_green_point": 8,
+      "discount": 0.10,
+      "url": "//assets.iceland.co.uk/i/iceland/arctic_royal_4_atlantic_salmon_fillets_520g_76676_T1.jpg"
+    },
+    {
+      "name": "Organic Spinach",
+      "id": 107,
+      "price": 3.75,
+      "green": true,
+      "sustainable": true,
+      "seasonal": true,
+      "local": true,
+      "eco_friendly_green_point": 9,
+      "discount": 1.20,
+      "url": "//assets.iceland.co.uk/i/iceland/little_leaf_co_mild_baby_spinach_100g_94729_T596.jpg"
+    },
+    {
+      "name": "Locally Sourced Honey",
+      "id": 108,
+      "price": 6.50,
+      "green": true,
+      "sustainable": true,
+      "seasonal": true,
+      "local": true,
+      "eco_friendly_green_point": 7,
+      "discount": 0.10,
+      "url": "//assets.iceland.co.uk/i/iceland/hilltop_value_clear_honey_250g_89070_T1.jpg"
+    },
+    {
+      "name": "Plant-Based Burger",
+      "id": 109,
+      "price": 3.99,
+      "green": true,
+      "sustainable": true,
+      "seasonal": false,
+      "local": false,
+      "eco_friendly_green_point": 8,
+      "discount": 0.15,
+      "url": "//assets.iceland.co.uk/i/iceland/frys_4_plant_based_chicken_style_burgers_320g_69230_T1.jpg"
+    },
+    {
+      "name": "Organic Blueberries",
+      "id": 110,
+      "price": 4.50,
+      "green": true,
+      "sustainable": true,
+      "seasonal": true,
+      "local": false,
+      "eco_friendly_green_point": 7,
+      "discount": 0.10,
+      "url": "//assets.iceland.co.uk/i/iceland/iceland_blueberries_150g_74443.jpg"
+    }
+  ]
+  
 
-const greenproducts = [
-      {
-        "name": "Organic Bananas",
-        "id": 101,
-        "sustainable": true,
-        "seasonal": false,
-        "local": false,
-        "eco_friendly_green_point": 8,
-        "discount": 0.15,
-        "url": //assets.iceland.co.uk/i/iceland/fyffes_premium_bananas_89322_T1.jpg
-      },
-      {
-        "name": "Grass-fed Beef",
-        "id": 102,
-        "sustainable": false,
-        "seasonal": true,
-        "local": false,
-        "eco_friendly_green_point": 9,
-        "discount": 0.10
-      },
-      {
-        "name": "Quinoa Salad",
-        "id": 103,
-        "sustainable": false,
-        "seasonal": true,
-        "local": true,
-        "eco_friendly_green_point": 7,
-        "discount": 0.20
-      },
-      {
-        "name": "Free-range Eggs",
-        "id": 104,
-        "sustainable": true,
-        "seasonal": false,
-        "local": true,
-        "eco_friendly_green_point": 6,
-        "discount": 0.10
-      },
-      {
-        "name": "Whole Grain Bread",
-        "id": 105,
-        "sustainable": true,
-        "seasonal": true,
-        "local": true,
-        "eco_friendly_green_point": 7,
-        "discount": 0.15
-      }
-    ]
-
-    export default greenproducts;
+    export default products;
   
