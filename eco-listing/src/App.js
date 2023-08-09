e41d85d6f879418e5ca59099972b1668ac0d0d98
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-08-09 10:47:25 +0100

    setup done

---
+++ eco-listing/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-xl md:text-[2rem] font-Jost font-bold text-blue-500 self-center">App</h1>
+    </div>
+  );
+}
+
+export default App;
