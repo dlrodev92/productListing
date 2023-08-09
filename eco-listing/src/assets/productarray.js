318249c3e7b1daeb137a55eb540aff483a9440f3
saynab <saynabd4@gmail.com>
2023-08-09 11:57:08 +0100

    20 objects

--- eco-listing/src/assets/productarray.js
+++ eco-listing/src/assets/productarray.js
@@ -1,52 +1,136 @@
 
-const greenproducts = [
+const greenproducts = [    
       {
-        "name": "Organic Bananas",
-        "id": 101,
-        "sustainable": true,
-        "seasonal": false,
-        "local": false,
-        "eco_friendly_green_point": 8,
-        "discount": 0.15,
-        "url": //assets.iceland.co.uk/i/iceland/fyffes_premium_bananas_89322_T1.jpg
-      },
-      {
-        "name": "Grass-fed Beef",
-        "id": 102,
-        "sustainable": false,
-        "seasonal": true,
-        "local": false,
-        "eco_friendly_green_point": 9,
-        "discount": 0.10
-      },
-      {
-        "name": "Quinoa Salad",
-        "id": 103,
-        "sustainable": false,
-        "seasonal": true,
-        "local": true,
-        "eco_friendly_green_point": 7,
-        "discount": 0.20
-      },
-      {
-        "name": "Free-range Eggs",
-        "id": 104,
-        "sustainable": true,
-        "seasonal": false,
-        "local": true,
-        "eco_friendly_green_point": 6,
-        "discount": 0.10
-      },
-      {
-        "name": "Whole Grain Bread",
+        "name": "Cheese",
         "id": 105,
+        "price": 1.99,
+        "green": false,
+        "sustainable": false,
+        "seasonal": false,
+        "local": false,
+        "eco_friendly_green_point": 7,
+        "discount": 0.15,
+        "url": "//assets.iceland.co.uk/i/iceland/iceland_mature_cheddar_220g_88833.jpg"
+      },
+      {
+        "name": "Milk",
+        "id": 105,
+        "price": 2.99,
+        "green": true,
         "sustainable": true,
         "seasonal": true,
         "local": true,
         "eco_friendly_green_point": 7,
-        "discount": 0.15
+        "discount": 0.15,
+        "url": "//assets.iceland.co.uk/i/iceland/iceland_british_semi_skimmed_milk_4_pints_2272l_22919.jpg"
+      },
+      {
+        "name": "Plums",
+        "id": 105,
+        "price": 0.99,
+        "green": false,
+        "sustainable": false,
+        "seasonal":false,
+        "local": false,
+        "eco_friendly_green_point": 7,
+        "discount": 0.15,
+        "url": "//assets.iceland.co.uk/i/iceland/iceland_plums_79326.jpg"
+      },
+      {
+        "name": "Pineapple",
+        "id": 105,
+        "price": 1.09,
+        "green": true,
+        "sustainable": true,
+        "seasonal": true,
+        "local": true,
+        "eco_friendly_green_point": 7,
+        "discount": 0.15,
+        "url": "//assets.iceland.co.uk/i/iceland/del_monte_pineapple_200g_92886_T1.jpg"
+      },
+      {
+        "name": "Walnuts",
+        "id": 105,
+        "price": 0.85,
+        "green": false,
+        "sustainable": false,
+        "seasonal": false,
+        "local": false,
+        "eco_friendly_green_point": 7,
+        "discount": 0.15,
+        "url":"//assets.iceland.co.uk/i/iceland/snacking_essentials_walnuts_200g_82274_T1.jpg"
+      },
+      {
+        "name": "Carrot Cake",
+        "id": 105,
+        "price": 1.25,
+        "green": false,
+        "sustainable": false,
+        "seasonal": false,
+        "local": false,
+        "eco_friendly_green_point": 0,
+        "discount": 0.15,
+        "url":"//assets.iceland.co.uk/i/iceland/carrs_delicious_carrot_cake_mix_425g_94418_T1.jpg"
+      },
+      {
+        "name": "Iceberg Lettuce",
+        "id": 105,
+        "price": 1.89,
+        "green": true,
+        "sustainable": true,
+        "seasonal": true,
+        "local": true,
+        "eco_friendly_green_point": 7,
+        "discount": 0.15,
+        "url": "//assets.iceland.co.uk/i/iceland/iceland_iceberg_lettuce_19705.jpg"
+      },
+      {
+        "name": "Frozen Fish Fingers",
+        "id": 105,
+        "price": 3.00,
+        "green": true,
+        "sustainable": true,
+        "seasonal": true,
+        "local": true,
+        "eco_friendly_green_point": 7,
+        "discount": 0.15,
+        "url": "//assets.iceland.co.uk/i/iceland/birds_eye_8_crispy_batter_fish_fingers_224g_57317_T596.jpg"
+      },
+      {
+        "name": "Salmon",
+        "id": 105,
+        "price": 10.00,
+        "green": true,
+        "sustainable": true,
+        "seasonal": true,
+        "local": true,
+        "eco_friendly_green_point": 7,
+        "discount": 0.15,
+        "url":"//assets.iceland.co.uk/i/iceland/iceland_boneless_2_atlantic_salmon_fillets_240g_77504.jpg"
+      },
+      {
+        "name": "Frozen Chips",
+        "id": 105,
+        "price": 1.50,
+        "green": false,
+        "sustainable": false,
+        "seasonal": false,
+        "local": false,
+        "eco_friendly_green_point": 7,
+        "discount": 0.15,
+        "url": "//assets.iceland.co.uk/i/iceland/mccain_quick_chips_crinkle_6_x_100g_600g_77778_T1.jpg"
       }
     ]
+
+
+
+
+
+
+
+
+
+
 
     export default greenproducts;
   
